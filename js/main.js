b1472928bc947fb80eb9a9944b67633df218c9c3
M003T <malekdu13012@gmail.com>
2021-10-17 16:24:23 +0200

    Improved Overall Design

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
     // sticky navigation menu
 
@@ -6,17 +6,54 @@ $(document).ready(function () {
 
     function navbarFixed() {
         if ($('.header_area').length) {
-            $(window).scroll(function () {
+            $(window).scroll(function() {
                 let scroll = $(window).scrollTop();
                 if (scroll >= nav_offset_top) {
                     $('.header_area .main-menu').addClass('navbar_fixed');
+                    $('.anchor').addClass('first');
                 } else {
                     $('.header_area .main-menu').removeClass('navbar_fixed');
+                    $('.anchor').removeClass('first');
                 }
             })
         }
     }
 
-    navbarFixed(); 
+    navbarFixed();
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 400) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+        console.log($(this).scrollTop());
+    });
+    $('#back-to-top').click(function() {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+    // Tooltips
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
 
 })
+
+// Progressbars
+/*
+var i = 0;
+function makeProgress(){
+    if(i < 100){
+        i = i + 1;
+        $(".progress-bar").css("width", i + "%").text(i + "%");
+    }
+
+    // Wait for sometime before running this script again
+    setTimeout("makeProgress()", 100);
+}
+makeProgress();
+*/
